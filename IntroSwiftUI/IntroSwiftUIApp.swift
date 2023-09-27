6e73698dce37a2dd2b0a94b9ff35cfd0cf74fd15
taokyone <antonsson.andreas@hotmail.com>
2023-09-27 09:13:18 +0200

    Initial Commit

---
+++ IntroSwiftUI/IntroSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroSwiftUIApp.swift
+//  IntroSwiftUI
+//
+//  Created by Andreas Antonsson on 2023-09-27.
+//
+
+import SwiftUI
+
+@main
+struct IntroSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
