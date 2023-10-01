75610b2e3189a9756eba2484a4779e5daf5fa145
taokyone <antonsson.andreas@hotmail.com>
2023-10-01 18:55:35 +0200

    fixing navigation

--- IntroSwiftUI/ContentView.swift
+++ IntroSwiftUI/ContentView.swift
@@ -12,42 +12,57 @@ struct Person: Identifiable {
     var firstName: String
     var lastName: String
 }
+// 11.30
+
 
 struct ContentView: View {
-    
+  
     let persons: [Person] = [Person(firstName: "Bruce", lastName: "Banner"), Person(firstName: "Steve", lastName: "Rodgers"), Person(firstName: "Black", lastName: "Widow")]
     
     var body: some View {
-        ZStack{ Color.brown.ignoresSafeArea()
-            VStack {
-                Image(systemName: "globe").resizable().frame(width: 64, height: 64)
-                    .imageScale(.large)
-                    .foregroundColor(.cyan)
-                //Spacer()
-                Text("Hello,World").foregroundColor(.white).bold().font(.largeTitle)
-                
-                
-                List(persons) { person in
-                    Text("\(person.firstName) \(person.lastName)")
-                }
-                
-                
-                ForEach(persons) { person in
-                    PersonView(person: person)
-                }
-                
-                
-                HStack {
-                    Button(action: {
-                        print("Button pressed")}, label: {
-                            Text("Press me").foregroundColor(.white).frame(width: 200, height: 40).background(.black).cornerRadius(10)
+        
+        NavigationStack {
+        
+            ZStack{ Color.brown.ignoresSafeArea()
+                VStack(alignment: .leading) {
+                    Image(systemName: "globe").resizable().frame(width: 64, height: 64)
+                        .imageScale(.large)
+                        .foregroundColor(.cyan)
+                    //Spacer()
+                    Text("Hello,World").foregroundColor(.white).bold().font(.largeTitle)
+                    
+                    /*
+                    List(persons) { person in
+                        Text("\(person.firstName) \(person.lastName)")
+                    }
+                    */
+                    
+                    // Widget
+                    VStack(alignment: .leading, spacing: 10) {
+                        ForEach(persons) { person in
+                            PersonView(person: person)
+                        }
+                    }
+                    
+                    
+                    
+                    
+                    // To be able to navigate, we need a navigation link. Navigationlink is like a button
+                    NavigationLink(destination: SecondScreenView(), label: {
+                        HStack {
+                            Text("Navigate").foregroundColor(.white).frame(width: 200, height: 40).background(.black).cornerRadius(10).padding(10)
+                        }
                     })
-                    Text("text!").foregroundColor(.white).font(.caption).padding()
-                }
-
+                   /* Button
+                        Button(action: {
+                            print("Button pressed")}, label:
+                                {
+                                HStack {
+                                    Text("Navigate").foregroundColor(.white).frame(width: 200, height: 40).background(.black).cornerRadius(10).padding(10)
+                                }
+                            })  */
+                    }
             }
-
-            
         }
     }
 }
@@ -55,19 +70,35 @@ struct ContentView: View {
 struct PersonView: View {
     
     var person: Person
-    
+
+     
     var body: some View {
-        HStack {
+        HStack(alignment: .center) {
             Image(systemName: "person.circle")
-            Text("\(person.firstName))")
+            Text("\(person.firstName) \(person.lastName)").foregroundColor(.black)
             
         }
         
     }
 }
 
+struct SecondScreenView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to the Second view")
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        //SecondScreenView()
+        //PersonView(person: Person(firstName: "Andreas", lastName: "Antonsson"))
+    
     }
 }
+
+// En struct kan inte ärva från en annan struct , som en klass kan. Struct behöver ingen init som en klass behöver.
+
+ 
